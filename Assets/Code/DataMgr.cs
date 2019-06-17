687fb116819514df75664b07415faa889a1ef464
dennis1009 <42278396+dennis1009@users.noreply.github.com>
2019-06-17 10:13:12 +0800

    限制0元买卖
    限制0元买卖，调整出售方法
    todo 持仓价格还是错的

--- Assets/Code/DataMgr.cs
+++ Assets/Code/DataMgr.cs
@@ -21,18 +21,27 @@ public class DataMgr  {
     public void Init ()
     {
 
-        startprice = Random.Range(10, 20);
+        startprice = Random.Range(100, 200);
         curprice = startprice;
 
 	}
 
     public void CalcPrice()
     {
-        curprice += Random.Range(-10, 10);
+        curprice += Random.Range(-50, 50);
+        if (curprice<0)
+        {
+            curprice = 0;
+        }
     }
 
     public void BuyAction(bool isall)
     {
+        if (curprice <= 0)//0元保护
+        {
+            Debug.LogError("0元禁止出售或购买");
+            return;
+        }
         UIMgr uiMgr = Object.FindObjectOfType<UIMgr>();
         int nowbuyCount = isall ? curmoney / curprice : 1;
         int nowbuyPrice = curprice;
@@ -43,10 +52,16 @@ public class DataMgr  {
         uiMgr.m_count.text = haveCount.ToString();
         uiMgr.m_money.text = curmoney.ToString();
         uiMgr.m_avg.text = avgPrice.ToString();
+        Debug.LogError(avgPrice);
     }
 
     public void SellAction(bool isall)
     {
+        if (curprice <= 0)//0元保护
+        {
+            Debug.LogError("0元禁止出售或购买");
+            return;
+        }
         UIMgr uiMgr = Object.FindObjectOfType<UIMgr>();
         int nowsellCount = isall ? haveCount : 1;
         int nowsellPrice = curprice;
@@ -56,5 +71,6 @@ public class DataMgr  {
         uiMgr.m_count.text = haveCount.ToString();
         uiMgr.m_money.text = curmoney.ToString();
         uiMgr.m_avg.text = avgPrice.ToString();
+        Debug.LogError(avgPrice);
     }
 }
